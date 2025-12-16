d51335661c418397f752e9811f2c1ee0aed84082
gitJackG <j.griffithsrico@gmail.com>
2025-12-16 11:38:21 +0000

    minimal ui change

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,25 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
 import "chartkick"
 import "Chart.bundle"
+
+Chart.defaults.font.family = "'Space Grotesk', sans-serif";
+const chartBackgroundPlugin = {
+  id: 'customBackground',
+  beforeDraw: (chart) => {
+    const { ctx, chartArea } = chart;
+    ctx.save();
+    ctx.fillStyle = "rgb(45, 63, 102)";
+    ctx.fillRect(
+      chartArea.left,
+      chartArea.top,
+      chartArea.width,
+      chartArea.height
+    );
+    ctx.restore();
+  }
+};
+Chart.register(chartBackgroundPlugin);
+
